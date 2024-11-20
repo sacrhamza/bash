f84f516e54f61443ac8f0d52050d6517e1677065
Hamza Sacr <hsacr@e1r10p6.1337.ma>
2024-11-20 20:47:56 +0100

    push

--- bashrc
+++ bashrc
@@ -1,3 +1,2 @@
 source ~/.alias.sh
-export CLICOLOR=1
 PS1='\n\[\e[01;34m\]$?\[\e[00m\] \[\e[01;32m\]hamza@fatalism\[\e[00m\]\[\e[01;36m\] \w\[\e[00m\]\[\e[01;34m\]\n❯ \[\e[00m\]'
--- requirements.sh
+++ requirements.sh
@@ -2,4 +2,5 @@
 sudo apt install sudo -y
 sudo apt update && sudo apt upgrade - y
 sudo apt install tmux git ssh apache2 hyperfine tldr yt-dlp ufw net-tools python3-pip -y
+sudo apt install btop htop -y
 sudo apt install finger -y
