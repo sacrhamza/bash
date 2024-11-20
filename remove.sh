ff959a4af65c8acb55493d5712fce94950d639fb
Hamza Sacr <hsacr@e1r10p6.1337.ma>
2024-11-20 18:14:10 +0100

    hey

--- remove.sh
+++ remove.sh
@@ -4,4 +4,4 @@ sudo apt update && sudo apt upgrade
 sudo apt purge tmux git ssh apache2 hyperfine tldr yt-dlp ufw net-tools python3-pip
 sudo apt purge finger
 mv ~/bashrc ~/.bashrc
-rm -rf .alias.sh
+rm -rf ~/.alias.sh
