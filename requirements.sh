b7e03fb9a321b70aa46e461b73dc6e3b116340fb
Hamza Sacr <hsacr@e1r10p6.1337.ma>
2024-11-20 21:05:40 +0100

    hey

--- requirements.sh
+++ requirements.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-sudo apt install sudo -y
-sudo apt update && sudo apt upgrade - y
-sudo apt install tmux git ssh apache2 hyperfine tldr yt-dlp ufw net-tools python3-pip -y
-sudo apt install btop htop -y
-sudo apt install finger -y
+
+packages=(sudo tmux git ssh apache2 hyperfine tldr yt-dlp ufw net-tools python3-pip btop htop finger hollywood)
+
+for i in ${packages[@]}
+do
+	if [[ -z $(which $i) ]]
+	then
+		sudo apt install $i -y
+	fi
+done
