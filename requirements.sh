23d483ecaa73f6230048f1756a644ef81bf92139
sacrhamza <sacrhamzaschool@gmail.com>
2024-11-27 14:08:29 +0100

    hey

--- requirements.sh
+++ requirements.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-packages=(sudo tmux git ssh apache2 hyperfine tldr yt-dlp ufw net-tools python3-pip btop htop finger hollywood)
+packages=(vim tmux git ssh apache2 hyperfine tldr yt-dlp ufw net-tools python3-pip btop htop finger hollywood)
 
 for i in ${packages[@]}
 do
-	if [[ -z $(which $i) ]]
-	then
-		echo $i
-		echo hey hey hey hey 
-		sleep 4
-		sudo apt install $i -y
-	fi
+	sudo apt install $i -y
 done
